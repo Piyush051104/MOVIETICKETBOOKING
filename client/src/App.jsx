1e73b79fb9d1203beeaa67aee6e4cbe08a9b01c3
Piyush051104 <piyushgup1023@example.com>
2026-02-15 11:46:26 +0530

    routing of pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import Mybookings from './pages/Mybookings'
+import Favourite from './pages/Favourite'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/movies' element={<Movies/>} />
+        <Route path='/movies/:id' element={<MovieDetails/>} />
+        <Route path='/movies/:id/date' element={<SeatLayout/>} />
+        <Route path='/my-bookings' element={<Mybookings/>} />
+        <Route path='/favourites' element={<Favourite/>} />
+      </Routes>
+    </>
+    
   )
 }
 
