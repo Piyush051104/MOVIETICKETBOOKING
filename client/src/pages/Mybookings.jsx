d5158b8e43aa02e1eb8864c471ee361489a1d2e1
Piyush051104 <piyushgup1023@example.com>
2026-02-19 16:39:15 +0530

    setlayout page done

--- client/src/pages/Mybookings.jsx
+++ client/src/pages/Mybookings.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { dummyBookingData } from '../assets/assets'
+import Loading from '../components/Loading'
 
 const Mybookings = () => {
-  return (
-    <div>Mybookings</div>
-  )
+  const currency = import.meta.env.VITE_CURRENCY
+  const [bookings, setBookings] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getMyBookings = async() => {
+    setBookings(dummyBookingData)
+    setIsLoading(false);
+  }
+
+  useEffect(()=>{
+    getMyBookings()
+  },[])
+  return !isLoading (
+    <div>
+      
+    </div>
+  ) : <Loading/>
 }
 
 export default Mybookings
