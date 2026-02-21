48746590030fe15c42aaaf287824e3353e03d616
Piyush051104 <piyushgup1023@gmail.com>
2026-02-21 12:22:06 +0530

    vercel config

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,6 +1,6 @@
 
 import { Inngest } from "inngest";
-import User from "../model/User";
+import User from "../model/User.js";
 
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 const syncUserCreation = inngest.createFunction(
