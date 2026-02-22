4ccb87268287042a1e1c600aae0f04956c68c0e9
Piyush051104 <piyushgup1023@gmail.com>
2026-02-22 15:19:49 +0530

    database movie add

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest,functions } from "./inngest/index.js";
+import showRouter from './routes/showRoutes.js';
 
 const app = express();
 const port = 3000;
@@ -16,5 +17,7 @@ app.use(clerkMiddleware())
 
 app.get('/',(req,res) => res.send('server is live!'))
 app.use('/api/inngest',serve({client:inngest,functions}));
+app.use('/api/show',showRouter)
+
 
 app.listen(3000,()=> console.log(`server is listening at http://localhost:${port}`));
