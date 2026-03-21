3c3d3623522628513bc15b8147461107b7c661f9
Piyush051104 <piyushgup1023@gmail.com>
2026-03-21 22:49:43 +0530

    mai.jsx fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { AppProvider } from './context/AppContext.jsx'
-import { assets } from '../assets/assets'
+
 
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
